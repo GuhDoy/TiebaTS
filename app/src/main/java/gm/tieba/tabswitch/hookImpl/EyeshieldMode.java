c062d66973715193f6114146caba0c961accfe2b
GM <897662476@qq.com>
2021-03-17 18:23:28 +0800

    fix EyeshieldMode on 12.4.0.0

--- app/src/main/java/gm/tieba/tabswitch/hookImpl/EyeshieldMode.java
+++ app/src/main/java/gm/tieba/tabswitch/hookImpl/EyeshieldMode.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.view.View;
 
 import de.robv.android.xposed.XC_MethodHook;
@@ -19,35 +20,34 @@ public class EyeshieldMode extends Hook {
         SharedPreferences sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
         if (!sharedPreferences.getBoolean("key_is_follow_system_mode", false)) return;
         savedUiMode = DisplayHelper.isLightMode(context);
-        XposedHelpers.findAndHookMethod("com.baidu.tbadk.core.TbadkCoreApplication", classLoader, "setSkinTypeValue", int.class, new XC_MethodHook() {
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                if (!DisplayHelper.isLightMode(context)) {
-                    savedUiMode = DisplayHelper.isLightMode(context);
-                    param.args[0] = 1;
-                }
-            }
-        });
         XposedHelpers.findAndHookMethod("com.baidu.tieba.tblauncher.MainTabActivity", classLoader, "onConfigurationChanged", Configuration.class, new XC_MethodHook() {
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Activity activity = (Activity) param.thisObject;
-                if (DisplayHelper.isLightMode(activity) && savedUiMode != DisplayHelper.isLightMode(activity)) {
+                if (savedUiMode != DisplayHelper.isLightMode(activity)) {
                     Intent intent = new Intent().setClassName(activity, "com.baidu.tieba.setting.more.MoreActivity");
                     activity.startActivity(intent);
                 }
             }
         });
-        XposedHelpers.findAndHookMethod("com.baidu.tieba.setting.more.MoreActivity", classLoader, "onResume", new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("com.baidu.tieba.setting.more.MoreActivity", classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Activity activity = (Activity) param.thisObject;
-                if (DisplayHelper.isLightMode(activity) && savedUiMode != DisplayHelper.isLightMode(activity)) {
+                if (savedUiMode != DisplayHelper.isLightMode(activity)) {
                     savedUiMode = DisplayHelper.isLightMode(activity);
                     View itemSwitch = activity.findViewById(classLoader.loadClass("com.baidu.tieba.R$id").getField("item_switch").getInt(null));
                     Class<?> BdSwitchView = classLoader.loadClass("com.baidu.adp.widget.BdSwitchView.BdSwitchView");
-                    try {
-                        BdSwitchView.getDeclaredMethod("turnOff").invoke(itemSwitch);
-                    } catch (NoSuchMethodException e) {
-                        BdSwitchView.getDeclaredMethod("f").invoke(itemSwitch);
-                    }
+                    if (DisplayHelper.isLightMode(activity))
+                        try {
+                            BdSwitchView.getDeclaredMethod("turnOff").invoke(itemSwitch);
+                        } catch (NoSuchMethodException e) {
+                            BdSwitchView.getDeclaredMethod("f").invoke(itemSwitch);
+                        }
+                    else
+                        try {
+                            BdSwitchView.getDeclaredMethod("turnOn").invoke(itemSwitch);
+                        } catch (NoSuchMethodException e) {
+                            BdSwitchView.getDeclaredMethod("i").invoke(itemSwitch);
+                        }
                     activity.finish();
                 }
             }
