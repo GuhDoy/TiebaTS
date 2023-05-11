ec0dd26e1ef0aada9a14619e1c9e55453b9557e2
klxiaoniu <klxiaoniu@proton.me>
2023-05-11 10:43:42 +0800

    opt getTbSkin

--- app/src/main/java/gm/tieba/tabswitch/util/DisplayUtils.java
+++ app/src/main/java/gm/tieba/tabswitch/util/DisplayUtils.java
@@ -5,7 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 
-public class DisplayUtils {
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import gm.tieba.tabswitch.XposedContext;
+
+public class DisplayUtils extends XposedContext {
     public static boolean isLightMode(final Context context) {
         return (context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK)
                 == Configuration.UI_MODE_NIGHT_NO;
@@ -22,19 +26,29 @@ public class DisplayUtils {
     }
 
     public static String getTbSkin(final Context context) {
-        final var settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
-        if (settings.getBoolean("key_is_follow_system_mode", false)) {
-            return isLightMode(context) ? "" : "_2";
-        } else {
-            final var commonSettings = context.getSharedPreferences(
-                    "common_settings", Context.MODE_PRIVATE);
-            switch (commonSettings.getString("skin_", "0")) {
-                case "4":
-                    return "_2";
-                case "0":
-                default:
-                    return "";
+        //Lcom/baidu/tbadk/core/TbadkCoreApplication;->getSkinType()I
+        int skinType;
+        try {
+            Object instance = XposedHelpers.callStaticMethod(XposedHelpers.findClass("com.baidu.tbadk.core.TbadkCoreApplication", sClassLoader), "getInst");
+            skinType = (int) XposedHelpers.callMethod(instance, "getSkinType");
+        } catch (Exception e) {
+            XposedBridge.log(e);
+            final var settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+            if (settings.getBoolean("key_is_follow_system_mode", false)) {
+                return isLightMode(context) ? "" : "_2";
+            } else {
+                final var commonSettings = context.getSharedPreferences(
+                        "common_settings", Context.MODE_PRIVATE);
+                skinType = Integer.parseInt((commonSettings.getString("skin_", "0")));
             }
+        }
+        switch (skinType) {
+            case 1:
+            case 4:
+                return "_2";
+            case 0:
+            default:
+                return "";
         }
     }
 
