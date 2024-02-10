64e38506996536d5d33b8b234b9c4d8eab96bb9f
Zhaoqian Wang <zhw173@g.ucla.edu>
2024-02-09 17:39:06 -0800

    fix: better matcher for PurgeEnter

--- app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PurgeEnter.java
+++ app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PurgeEnter.java
@@ -10,16 +10,14 @@ import androidx.annotation.NonNull;
 import java.lang.reflect.Modifier;
 import java.util.List;
 
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import gm.tieba.tabswitch.XposedContext;
 import gm.tieba.tabswitch.dao.AcRules;
 import gm.tieba.tabswitch.hooker.IHooker;
 import gm.tieba.tabswitch.hooker.Obfuscated;
 import gm.tieba.tabswitch.hooker.deobfuscation.Matcher;
-import gm.tieba.tabswitch.hooker.deobfuscation.SmaliMatcher;
+import gm.tieba.tabswitch.hooker.deobfuscation.ResIdentifierMatcher;
 import gm.tieba.tabswitch.hooker.deobfuscation.StringMatcher;
 import gm.tieba.tabswitch.util.ReflectUtils;
 
@@ -35,7 +33,7 @@ public class PurgeEnter extends XposedContext implements IHooker, Obfuscated {
         return List.of(
                 new StringMatcher("c/f/forum/getRecommendForumData"),
                 new StringMatcher("enter_forum_login_tip"),
-                new SmaliMatcher("Lcom/baidu/adp/widget/ListView/BdListView;->setNextPage(Lcom/baidu/tieba/di;)V")
+                new ResIdentifierMatcher("tbds400", "dimen")
         );
     }
 
@@ -54,40 +52,38 @@ public class PurgeEnter extends XposedContext implements IHooker, Obfuscated {
                 case "enter_forum_login_tip":
                     mEnterForumAdViewClassName = clazz;
                     break;
-                case "Lcom/baidu/adp/widget/ListView/BdListView;->setNextPage(Lcom/baidu/tieba/di;)V":
+                case "dimen.tbds400":
                     if (clazz.equals(mEnterForumAdViewClassName)) {
-                        if (XposedHelpers.findMethodExact(clazz, sClassLoader, method).getParameterTypes().length == 0) {
-                            XposedHelpers.findAndHookMethod(clazz, sClassLoader, method, new XC_MethodReplacement() {
-                                        @Override
-                                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                                            Object pbListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.tbadk.core.view.PbListView");
+                        XposedHelpers.findAndHookMethod(clazz, sClassLoader, method, new XC_MethodReplacement() {
+                            @Override
+                            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                                Object pbListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.tbadk.core.view.PbListView");
 
-                                            String viewConstructorName = "";
-                                            for (final var method : XposedHelpers.findClass("com.baidu.tbadk.core.view.PbListView", sClassLoader).getSuperclass().getDeclaredMethods()) {
-                                                if (method.getReturnType().toString().endsWith("View") && !Modifier.isAbstract(method.getModifiers())) {
-                                                    viewConstructorName = method.getName();
-                                                    break;
-                                                }
-                                            }
-                                            View view = (View) XposedHelpers.callMethod(pbListView, viewConstructorName);
+                                String viewConstructorName = "";
+                                for (final var method : XposedHelpers.findClass("com.baidu.tbadk.core.view.PbListView", sClassLoader).getSuperclass().getDeclaredMethods()) {
+                                    if (method.getReturnType().toString().endsWith("View") && !Modifier.isAbstract(method.getModifiers())) {
+                                        viewConstructorName = method.getName();
+                                        break;
+                                    }
+                                }
+                                View view = (View) XposedHelpers.callMethod(pbListView, viewConstructorName);
 
-                                            if (view.getParent() == null) {
-                                                Object bdListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.adp.widget.ListView.BdListView");
-                                                XposedHelpers.callMethod(bdListView, "setNextPage", pbListView);
-                                            }
+                                if (view.getParent() == null) {
+                                    Object bdListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.adp.widget.ListView.BdListView");
+                                    XposedHelpers.callMethod(bdListView, "setNextPage", pbListView);
+                                }
 
-                                            LinearLayout linearLayout = (LinearLayout) ReflectUtils.getObjectField(pbListView, "android.widget.LinearLayout");
-                                            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(linearLayout.getLayoutParams());
+                                LinearLayout linearLayout = (LinearLayout) ReflectUtils.getObjectField(pbListView, "android.widget.LinearLayout");
+                                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(linearLayout.getLayoutParams());
 
-                                            if (mInitLayoutHeight == -1){
-                                                mInitLayoutHeight = layoutParams.height + mLayoutOffset;
-                                            }
-                                            layoutParams.height = mInitLayoutHeight;
-                                            linearLayout.setLayoutParams(layoutParams);
-                                            return null;
-                                        }
-                            });
-                        }
+                                if (mInitLayoutHeight == -1){
+                                    mInitLayoutHeight = layoutParams.height + mLayoutOffset;
+                                }
+                                layoutParams.height = mInitLayoutHeight;
+                                linearLayout.setLayoutParams(layoutParams);
+                                return null;
+                            }
+                        });
                     }
                     break;
             }
