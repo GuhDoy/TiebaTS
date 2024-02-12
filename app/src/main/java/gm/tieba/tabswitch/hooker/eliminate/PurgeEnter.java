a8ac19406acba764f6f079efd5426ae9f30664e2
Zhaoqian Wang <zhw173@g.ucla.edu>
2024-02-11 19:46:35 -0800

    feat: better recommend hot forum removal

--- app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PurgeEnter.java
+++ app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PurgeEnter.java
@@ -1,11 +1,13 @@
 package gm.tieba.tabswitch.hooker.eliminate;
 
-import android.app.AndroidAppHelper;
-import android.content.res.Resources;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+
+import org.luckypray.dexkit.query.matchers.ClassMatcher;
+import org.luckypray.dexkit.query.matchers.MethodMatcher;
+import org.luckypray.dexkit.query.matchers.MethodsMatcher;
 
 import java.lang.reflect.Modifier;
 import java.util.List;
@@ -16,6 +18,8 @@ import gm.tieba.tabswitch.XposedContext;
 import gm.tieba.tabswitch.dao.AcRules;
 import gm.tieba.tabswitch.hooker.IHooker;
 import gm.tieba.tabswitch.hooker.Obfuscated;
+import gm.tieba.tabswitch.hooker.deobfuscation.MethodNameMatcher;
+import gm.tieba.tabswitch.hooker.deobfuscation.PackageMatcher;
 import gm.tieba.tabswitch.hooker.deobfuscation.Matcher;
 import gm.tieba.tabswitch.hooker.deobfuscation.ResIdentifierMatcher;
 import gm.tieba.tabswitch.hooker.deobfuscation.StringMatcher;
@@ -28,63 +32,83 @@ public class PurgeEnter extends XposedContext implements IHooker, Obfuscated {
         return "purge_enter";
     }
 
+    private final ClassMatcher mRecForumClassMatcher =
+            ClassMatcher.create().methods(
+                    MethodsMatcher.create().methods(
+                            List.of(MethodMatcher.create().usingStrings("enter_forum_login_tip"))));
+    private int mInitLayoutHeight = -1;
+    private final int mLayoutOffset = (int) ReflectUtils.getDimen("tbds50");
+    private String mRecForumClassName, mRecForumSetNextPageMethodName;
+
     @Override
     public List<? extends Matcher> matchers() {
         return List.of(
-                new StringMatcher("c/f/forum/getRecommendForumData"),
-                new StringMatcher("enter_forum_login_tip"),
-                new ResIdentifierMatcher("tbds400", "dimen")
+                new PackageMatcher(mRecForumClassMatcher,
+                        "enter_forum_login_tip",
+                        new ResIdentifierMatcher("tbds400", "dimen")),
+                new PackageMatcher(mRecForumClassMatcher,
+                        "enter_forum_login_tip",
+                        new MethodNameMatcher("onSuccess"))
         );
     }
 
-    private String mEnterForumAdViewClassName;
-    private int mInitLayoutHeight = -1;
-    private final Resources currentRes = AndroidAppHelper.currentApplication().getApplicationContext().getResources();
-    private final int mLayoutOffset = currentRes.getDimensionPixelSize(currentRes.getIdentifier("tbds50", "dimen", "com.baidu.tieba"));
-
     @Override
     public void hook() throws Throwable {
+        XposedHelpers.findAndHookMethod(
+                "com.baidu.tieba.enterForum.recforum.message.RecommendForumRespondedMessage",
+                sClassLoader,
+                "getRecommendForumData",
+                XC_MethodReplacement.returnConstant(null));
+
         AcRules.findRule(matchers(), (matcher, clazz, method) -> {
             switch (matcher) {
-                case "c/f/forum/getRecommendForumData":
-                    XposedHelpers.findAndHookMethod(clazz, sClassLoader, method, XC_MethodReplacement.returnConstant(null));
-                    break;
-                case "enter_forum_login_tip":
-                    mEnterForumAdViewClassName = clazz;
-                    break;
-                case "dimen.tbds400":
-                    if (clazz.equals(mEnterForumAdViewClassName)) {
-                        XposedHelpers.findAndHookMethod(clazz, sClassLoader, method, new XC_MethodReplacement() {
-                            @Override
-                            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                                Object pbListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.tbadk.core.view.PbListView");
+                case "enter_forum_login_tip/dimen.tbds400":
+                    mRecForumClassName = clazz;
+                    mRecForumSetNextPageMethodName = method;
+                    XposedHelpers.findAndHookMethod(clazz, sClassLoader, method, new XC_MethodReplacement() {
+                        @Override
+                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                            Object pbListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.tbadk.core.view.PbListView");
 
-                                String viewConstructorName = "";
-                                for (final var method : XposedHelpers.findClass("com.baidu.tbadk.core.view.PbListView", sClassLoader).getSuperclass().getDeclaredMethods()) {
-                                    if (method.getReturnType().toString().endsWith("View") && !Modifier.isAbstract(method.getModifiers())) {
-                                        viewConstructorName = method.getName();
-                                        break;
-                                    }
+                            String viewConstructorName = "";
+                            for (final var method : XposedHelpers.findClass("com.baidu.tbadk.core.view.PbListView", sClassLoader).getSuperclass().getDeclaredMethods()) {
+                                if (method.getReturnType().toString().endsWith("View") && !Modifier.isAbstract(method.getModifiers())) {
+                                    viewConstructorName = method.getName();
+                                    break;
                                 }
-                                View view = (View) XposedHelpers.callMethod(pbListView, viewConstructorName);
-
-                                if (view.getParent() == null) {
-                                    Object bdListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.adp.widget.ListView.BdListView");
-                                    XposedHelpers.callMethod(bdListView, "setNextPage", pbListView);
-                                }
-
-                                LinearLayout linearLayout = (LinearLayout) ReflectUtils.getObjectField(pbListView, "android.widget.LinearLayout");
-                                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(linearLayout.getLayoutParams());
-
-                                if (mInitLayoutHeight == -1){
-                                    mInitLayoutHeight = layoutParams.height + mLayoutOffset;
-                                }
-                                layoutParams.height = mInitLayoutHeight;
-                                linearLayout.setLayoutParams(layoutParams);
-                                return null;
                             }
-                        });
-                    }
+                            View view = (View) XposedHelpers.callMethod(pbListView, viewConstructorName);
+
+                            if (view.getParent() == null) {
+                                Object bdListView = ReflectUtils.getObjectField(param.thisObject, "com.baidu.adp.widget.ListView.BdListView");
+                                XposedHelpers.callMethod(bdListView, "setNextPage", pbListView);
+                            }
+
+                            LinearLayout linearLayout = (LinearLayout) ReflectUtils.getObjectField(pbListView, "android.widget.LinearLayout");
+                            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(linearLayout.getLayoutParams());
+
+                            if (mInitLayoutHeight == -1){
+                                mInitLayoutHeight = layoutParams.height + mLayoutOffset;
+                            }
+                            layoutParams.height = mInitLayoutHeight;
+                            linearLayout.setLayoutParams(layoutParams);
+                            return null;
+                        }
+                    });
+                    break;
+                case "enter_forum_login_tip/onSuccess":
+                    XposedHelpers.findAndHookMethod(clazz,
+                            sClassLoader,
+                            method,
+                            boolean.class,
+                            new XC_MethodReplacement() {
+                                @Override
+                                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                                    Object enterForumRec = ReflectUtils.getObjectField(param.thisObject, mRecForumClassName);
+                                    XposedHelpers.callMethod(enterForumRec, mRecForumSetNextPageMethodName);
+                                    return null;
+                                }
+                            });
                     break;
             }
         });
