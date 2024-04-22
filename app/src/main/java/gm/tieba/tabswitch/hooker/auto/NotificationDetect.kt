952575264f7da6fbac9101f3363a24f862bec4a1
mkx173 <zhw173@g.ucla.edu>
2024-04-22 00:43:32 -0700

    Migrate NotificationDetect to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/auto/NotificationDetect.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/auto/NotificationDetect.kt
@@ -1,32 +1,25 @@
-package gm.tieba.tabswitch.hooker.auto;
+package gm.tieba.tabswitch.hooker.auto
 
-import androidx.annotation.NonNull;
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.hooker.IHooker
 
-import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.hooker.IHooker;
-
-public class NotificationDetect extends XposedContext implements IHooker {
-    @NonNull
-    @Override
-    public String key() {
-        return "notification_detect";
+class NotificationDetect : XposedContext(), IHooker {
+    override fun key(): String {
+        return "notification_detect"
     }
 
-    public void hook() throws Throwable {
+    @Throws(Throwable::class)
+    override fun hook() {
         // 禁止检测通知开启状态
-        XposedHelpers.findAndHookMethod(
-                "androidx.core.app.NotificationManagerCompat",
-                sClassLoader,
-                "areNotificationsEnabled",
-                XC_MethodReplacement.returnConstant(true)
-        );
-        XposedHelpers.findAndHookMethod(
-                "com.baidu.tieba.push.PushSceneGroup",
-                sClassLoader,
-                "getLimit",
-                XC_MethodReplacement.returnConstant(0)
-        );
+        hookReplaceMethod(
+            "androidx.core.app.NotificationManagerCompat",
+            "areNotificationsEnabled"
+        ) { true }
+        hookReplaceMethod(
+            "com.baidu.tieba.push.PushSceneGroup",
+            "getLimit"
+        ) { 0 }
     }
 }
