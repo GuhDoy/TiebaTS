e0eb647e00c6f797cc835745f669015fe2d1a9ab
mkx173 <zhw173@g.ucla.edu>
2024-04-22 21:53:08 -0700

    Migrate ContentFilter to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/ContentFilter.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/ContentFilter.kt
@@ -1,56 +1,49 @@
-package gm.tieba.tabswitch.hooker.eliminate;
+package gm.tieba.tabswitch.hooker.eliminate
 
-import androidx.annotation.NonNull;
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.hooker.IHooker
+import gm.tieba.tabswitch.util.parsePbContent
 
-import java.util.List;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.hooker.IHooker;
-import gm.tieba.tabswitch.util.Parser;
-
-public class ContentFilter extends XposedContext implements IHooker, RegexFilter {
-
-    @NonNull
-    @Override
-    public String key() {
-        return "content_filter";
+class ContentFilter : XposedContext(), IHooker, RegexFilter {
+    override fun key(): String {
+        return "content_filter"
     }
 
-    @Override
-    public void hook() throws Throwable {
+    @Throws(Throwable::class)
+    override fun hook() {
         // 楼层
-        XposedHelpers.findAndHookMethod("tbclient.PbPage.DataRes$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                final var postList = (List<?>) XposedHelpers.getObjectField(param.thisObject, "post_list");
-                if (postList == null) return;
-                final var pattern = getPattern();
+        hookBeforeMethod(
+            "tbclient.PbPage.DataRes\$Builder",
+            "build", Boolean::class.javaPrimitiveType
+        ) { param ->
+            val postList = XposedHelpers.getObjectField(param.thisObject, "post_list") as? MutableList<*>
+            val pattern = getPattern()
+            postList?.removeIf { o: Any? ->
+                (XposedHelpers.getObjectField(o, "floor") as Int != 1
+                        && pattern.matcher(parsePbContent(o, "content")).find())
+            }
+        }
 
-                postList.removeIf(o -> ((Integer) XposedHelpers.getObjectField(o, "floor") != 1)
-                        && (pattern.matcher(Parser.parsePbContent(o, "content")).find()));
-            }
-        });
         // 楼中楼：[\u202e|\ud83c\udd10-\ud83c\udd89]
-        XposedHelpers.findAndHookMethod("tbclient.SubPost$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                final var subPostList = (List<?>) XposedHelpers.getObjectField(param.thisObject, "sub_post_list");
-                if (subPostList == null) return;
-                final var pattern = getPattern();
-                subPostList.removeIf(o -> pattern.matcher(Parser.parsePbContent(o, "content")).find());
-            }
-        });
+        hookBeforeMethod(
+            "tbclient.SubPost\$Builder",
+            "build", Boolean::class.javaPrimitiveType
+        ) { param ->
+            val subPostList = XposedHelpers.getObjectField(param.thisObject, "sub_post_list") as? MutableList<*>
+            val pattern = getPattern()
+            subPostList?.removeIf { o: Any? -> pattern.matcher(parsePbContent(o, "content")).find() }
+        }
+
         // 楼层回复
-        XposedHelpers.findAndHookMethod("tbclient.PbFloor.DataRes$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                final var subpostList = (List<?>) XposedHelpers.getObjectField(param.thisObject, "subpost_list");
-                if (subpostList == null) return;
-                final var pattern = getPattern();
-                subpostList.removeIf(o -> pattern.matcher(Parser.parsePbContent(o, "content")).find());
-            }
-        });
+        hookBeforeMethod(
+            "tbclient.PbFloor.DataRes\$Builder",
+            "build", Boolean::class.javaPrimitiveType
+        ) { param ->
+            val subPostList = XposedHelpers.getObjectField(param.thisObject, "subpost_list") as? MutableList<*>
+            val pattern = getPattern()
+            subPostList?.removeIf { o: Any? -> pattern.matcher(parsePbContent(o, "content")).find() }
+        }
     }
 }
