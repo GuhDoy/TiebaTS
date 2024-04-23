4ac8a7faf5d32813c9d7ef947b026f1440e9f140
mkx173 <zhw173@g.ucla.edu>
2024-04-22 23:33:16 -0700

    Migrate PersonalizedFilter to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PersonalizedFilter.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PersonalizedFilter.kt
@@ -1,39 +1,28 @@
-package gm.tieba.tabswitch.hooker.eliminate;
+package gm.tieba.tabswitch.hooker.eliminate
 
-import androidx.annotation.NonNull;
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.hooker.IHooker
+import gm.tieba.tabswitch.util.parsePbContent
 
-import java.util.List;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.hooker.IHooker;
-import gm.tieba.tabswitch.util.Parser;
-
-public class PersonalizedFilter extends XposedContext implements IHooker, RegexFilter {
-
-    @NonNull
-    @Override
-    public String key() {
-        return "personalized_filter";
+class PersonalizedFilter : XposedContext(), IHooker, RegexFilter {
+    override fun key(): String {
+        return "personalized_filter"
     }
 
-    @Override
-    public void hook() throws Throwable {
-        XposedHelpers.findAndHookMethod("tbclient.Personalized.DataRes$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                final var threadList = (List<?>) XposedHelpers.getObjectField(param.thisObject, "thread_list");
-                if (threadList == null) return;
-                final var pattern = getPattern();
-                threadList.removeIf(o -> {
-                    if (pattern.matcher(Parser.parsePbContent(o, "first_post_content")).find()) {
-                        return true;
-                    }
-
-                    return pattern.matcher((String) XposedHelpers.getObjectField(o, "title")).find();
-                });
+    @Throws(Throwable::class)
+    override fun hook() {
+        hookBeforeMethod(
+            "tbclient.Personalized.DataRes\$Builder",
+            "build", Boolean::class.javaPrimitiveType
+        ) { param ->
+            val threadList = XposedHelpers.getObjectField(param.thisObject, "thread_list") as? MutableList<*>
+            val pattern = getPattern()
+            threadList?.removeIf { threadItem ->
+                pattern.matcher(parsePbContent(threadItem, "first_post_content")).find() ||
+                        pattern.matcher(XposedHelpers.getObjectField(threadItem, "title") as? String ?: "").find()
             }
-        });
+        }
     }
 }
