805cd2664dea903cb51e958724de7a7f0af86c63
mkx173 <zhw173@g.ucla.edu>
2024-04-22 21:20:50 -0700

    Migrate OriginSrc to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/auto/OriginSrc.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/auto/OriginSrc.kt
@@ -1,173 +1,186 @@
-package gm.tieba.tabswitch.hooker.auto;
+package gm.tieba.tabswitch.hooker.auto
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.Uri
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.dao.AcRules
+import gm.tieba.tabswitch.dao.AcRules.findRule
+import gm.tieba.tabswitch.dao.Preferences.getBoolean
+import gm.tieba.tabswitch.hooker.IHooker
+import org.json.JSONObject
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.net.NetworkRequest;
-import android.net.Uri;
-
-import androidx.annotation.NonNull;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.dao.AcRules;
-import gm.tieba.tabswitch.dao.Preferences;
-import gm.tieba.tabswitch.hooker.IHooker;
-
-public class OriginSrc extends XposedContext implements IHooker {
-
-    @NonNull
-    @Override
-    public String key() {
-        return "origin_src";
-    }
-
-    private static boolean isHooked;
-    private static XC_MethodHook.Unhook picListUnhook;
-    private static XC_MethodHook.Unhook pbContentUnhook;
-    private static XC_MethodHook.Unhook mediaUnhook;
-    private static XC_MethodHook.Unhook picInfoUnhook;
-    private static XC_MethodHook.Unhook feedPicComponentUnhook;
-
-    private static void doHook() {
-        if (isHooked) return;
-        AcRules.findRule("pic_amount", (AcRules.Callback) (matcher, clazz, method) ->
-                picListUnhook = XposedHelpers.findAndHookMethod(clazz, sClassLoader, method, JSONObject.class, Boolean.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                        final JSONObject jsonObject = (JSONObject) param.args[0];
-                        final JSONArray picList = jsonObject.optJSONArray("pic_list");
-                        if (picList == null) return;
-                        for (int i = 0; i < picList.length(); i++) {
-                            final JSONObject pic = picList.optJSONObject(i);
-                            final JSONObject img = pic.getJSONObject("img");
-                            final JSONObject original = img.getJSONObject("original");
-                            original.put("big_cdn_src", original.getString("original_src"));
-                            img.put("original", original);
-                            pic.put("img", img);
-                            pic.put("show_original_btn", 0);
-                        }
-                        jsonObject.put("pic_list", picList);
-                    }
-                }));
-        pbContentUnhook = XposedHelpers.findAndHookMethod("tbclient.PbContent$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final XC_MethodHook.MethodHookParam param) throws Throwable {
-                XposedHelpers.setObjectField(param.thisObject, "show_original_btn", 0);
-                final String[] strings = new String[]{"big_cdn_src", "cdn_src", "cdn_src_active"};
-                for (final String string : strings) {
-                    XposedHelpers.setObjectField(param.thisObject, string, XposedHelpers
-                            .getObjectField(param.thisObject, "origin_src"));
-                }
-            }
-        });
-        mediaUnhook = XposedHelpers.findAndHookMethod("tbclient.Media$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final XC_MethodHook.MethodHookParam param) throws Throwable {
-                XposedHelpers.setObjectField(param.thisObject, "show_original_btn", 0);
-                final String[] strings = new String[]{"small_pic", "water_pic"};
-                for (final String string : strings) {
-                    XposedHelpers.setObjectField(param.thisObject, string, XposedHelpers
-                            .getObjectField(param.thisObject, "big_pic"));
-                }
-            }
-        });
-        picInfoUnhook = XposedHelpers.findAndHookMethod("tbclient.PicInfo$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final XC_MethodHook.MethodHookParam param) throws Throwable {
-                final String[] strings = new String[]{"small_pic_url", "big_pic_url"};
-                for (final String string : strings) {
-                    XposedHelpers.setObjectField(param.thisObject, string, XposedHelpers
-                            .getObjectField(param.thisObject, "origin_pic_url"));
-                }
-            }
-        });
-        feedPicComponentUnhook = XposedHelpers.findAndHookMethod("tbclient.FeedPicComponent$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final XC_MethodHook.MethodHookParam param) throws Throwable {
-                String schema = (String) XposedHelpers.getObjectField(param.thisObject, "schema");
-                String paramsJson = Uri.parse(schema).getQueryParameter("params");
-
-                JSONObject jsonObject = new JSONObject(paramsJson);
-                JSONObject pageParams = jsonObject.getJSONObject("pageParams");
-                JSONArray picDataList = pageParams.getJSONArray("pic_data_list");
-
-                for (int i = 0; i < picDataList.length(); i++) {
-                    JSONObject picData = picDataList.getJSONObject(i);
-                    String originPicUrl = picData.getString("origin_pic_url");
-                    picData.put("big_pic_url", originPicUrl);
-                    picData.put("small_pic_url", originPicUrl);
-                    picData.put("is_show_origin_btn", 0);
-                }
-
-                String modifiedUri = "tiebaapp://router/portal?params=" + jsonObject.toString();
-                XposedHelpers.setObjectField(param.thisObject, "schema", modifiedUri);
-            }
-        });
-        isHooked = true;
-    }
-
-    private static void doUnHook() {
-        if (!isHooked) return;
-        if (picListUnhook != null) {
-            picListUnhook.unhook();
-            picListUnhook = null;
-        }
-        if (pbContentUnhook != null) {
-            pbContentUnhook.unhook();
-            pbContentUnhook = null;
-        }
-        if (mediaUnhook != null) {
-            mediaUnhook.unhook();
-            mediaUnhook = null;
-        }
-        if (picInfoUnhook != null) {
-            picInfoUnhook.unhook();
-            picInfoUnhook = null;
-        }
-        if (feedPicComponentUnhook != null) {
-            feedPicComponentUnhook.unhook();
-            feedPicComponentUnhook = null;
-        }
-        isHooked = false;
+class OriginSrc : XposedContext(), IHooker {
+    override fun key(): String {
+        return "origin_src"
     }
 
     @SuppressLint("MissingPermission")
-    @Override
-    public void hook() throws Throwable {
-        if (Preferences.getBoolean("origin_src_only_wifi")) {
-            final NetworkCallbackImpl networkCallback = new NetworkCallbackImpl();
-            final NetworkRequest.Builder builder = new NetworkRequest.Builder();
-            final NetworkRequest request = builder.build();
-            final ConnectivityManager connMgr = (ConnectivityManager) getContext().getSystemService(
-                    Context.CONNECTIVITY_SERVICE);
-            if (connMgr != null) connMgr.registerNetworkCallback(request, networkCallback);
+    @Throws(Throwable::class)
+    override fun hook() {
+        if (getBoolean("origin_src_only_wifi")) {
+            val networkCallback = NetworkCallbackImpl()
+            val builder = NetworkRequest.Builder()
+            val request = builder.build()
+            val connMgr = getContext().getSystemService(
+                Context.CONNECTIVITY_SERVICE
+            ) as ConnectivityManager
+            connMgr.registerNetworkCallback(request, networkCallback)
         } else {
-            doHook();
+            doHook()
         }
     }
 
-    private static class NetworkCallbackImpl extends ConnectivityManager.NetworkCallback {
-        NetworkCallbackImpl() {
+    private class NetworkCallbackImpl : NetworkCallback() {
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            super.onCapabilitiesChanged(network, networkCapabilities)
+            if (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+            ) {
+                doHook()
+            } else {
+                doUnHook()
+            }
+        }
+    }
+
+    companion object {
+        private var isHooked = false
+        private var picListUnhook: XC_MethodHook.Unhook? = null
+        private var pbContentUnhook: XC_MethodHook.Unhook? = null
+        private var mediaUnhook: XC_MethodHook.Unhook? = null
+        private var picInfoUnhook: XC_MethodHook.Unhook? = null
+        private var feedPicComponentUnhook: XC_MethodHook.Unhook? = null
+        private fun doHook() {
+            if (isHooked) return
+
+            findRule("pic_amount") { _, clazz, method ->
+                picListUnhook = hookBeforeMethod(
+                    clazz, method, JSONObject::class.java, Boolean::class.javaObjectType
+                ) { param ->
+                    val jsonObject = param.args[0] as JSONObject
+
+                    jsonObject.optJSONArray("pic_list")?.let {
+                        for (i in 0 until it.length()) {
+                            val pic = it.optJSONObject(i)
+                            val img = pic.getJSONObject("img")
+                            val original = img.getJSONObject("original").apply {
+                                put("big_cdn_src", getString("original_src"))
+                            }
+                            img.put("original", original)
+                            pic.apply {
+                                put("img", img)
+                                put("show_original_btn", 0)
+                            }
+                        }
+                        jsonObject.put("pic_list", it)
+                    }
+                }
+
+                pbContentUnhook = hookBeforeMethod(
+                    "tbclient.PbContent\$Builder",
+                    "build", Boolean::class.javaPrimitiveType,
+                ) { param ->
+                    XposedHelpers.setObjectField(param.thisObject, "show_original_btn", 0)
+                    arrayOf("big_cdn_src", "cdn_src", "cdn_src_active").forEach {
+                        XposedHelpers.setObjectField(
+                            param.thisObject,
+                            it,
+                            XposedHelpers.getObjectField(param.thisObject, "origin_src")
+                        )
+                    }
+                }
+
+                mediaUnhook = hookBeforeMethod(
+                    "tbclient.Media\$Builder",
+                    "build", Boolean::class.javaPrimitiveType,
+                ) { param ->
+                    XposedHelpers.setObjectField(param.thisObject, "show_original_btn", 0)
+                    arrayOf("small_pic", "water_pic").forEach {
+                        XposedHelpers.setObjectField(
+                            param.thisObject,
+                            it,
+                            XposedHelpers.getObjectField(param.thisObject, "big_pic")
+                        )
+                    }
+                }
+
+                picInfoUnhook = hookBeforeMethod(
+                    "tbclient.PicInfo\$Builder",
+                    "build", Boolean::class.javaPrimitiveType,
+                ) { param ->
+                    arrayOf("small_pic_url", "big_pic_url").forEach {
+                        XposedHelpers.setObjectField(
+                            param.thisObject,
+                            it,
+                            XposedHelpers.getObjectField(param.thisObject, "origin_pic_url")
+                        )
+                    }
+                }
+
+                feedPicComponentUnhook = hookBeforeMethod(
+                    "tbclient.FeedPicComponent\$Builder",
+                    "build", Boolean::class.javaPrimitiveType,
+                ) { param ->
+                    val schema = XposedHelpers.getObjectField(param.thisObject, "schema") as String
+                    val paramsJson = Uri.parse(schema).getQueryParameter("params")
+
+                    paramsJson?.let {
+                        val jsonObject = JSONObject(it)
+                        val pageParams = jsonObject.getJSONObject("pageParams")
+                        val picDataList = pageParams.getJSONArray("pic_data_list")
+                        for (i in 0 until picDataList.length()) {
+                            val picData = picDataList.getJSONObject(i)
+                            val originPicUrl = picData.getString("origin_pic_url")
+                            picData.apply {
+                                put("big_pic_url", originPicUrl)
+                                put("small_pic_url", originPicUrl)
+                                put("is_show_origin_btn", 0)
+                            }
+                        }
+                        val modifiedUri = "tiebaapp://router/portal?params=$jsonObject"
+                        XposedHelpers.setObjectField(param.thisObject, "schema", modifiedUri)
+                    }
+                }
+
+                isHooked = true
+            }
         }
 
-        @Override
-        public void onCapabilitiesChanged(final Network network, final NetworkCapabilities networkCapabilities) {
-            super.onCapabilitiesChanged(network, networkCapabilities);
-            if (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-                    && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                doHook();
-            } else {
-                doUnHook();
+        private fun doUnHook() {
+            if (!isHooked) return
+            picListUnhook?.let {
+                it.unhook()
+                picListUnhook = null
             }
+            pbContentUnhook?.let {
+                it.unhook()
+                pbContentUnhook = null
+            }
+            mediaUnhook?.let {
+                it.unhook()
+                mediaUnhook = null
+            }
+            picInfoUnhook?.let {
+                it.unhook()
+                picInfoUnhook = null
+            }
+            feedPicComponentUnhook?.let {
+                it.unhook()
+                feedPicComponentUnhook = null
+            }
+            isHooked = false
+
         }
     }
 }
