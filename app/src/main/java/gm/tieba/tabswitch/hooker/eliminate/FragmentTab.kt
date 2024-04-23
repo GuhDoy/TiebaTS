d5c7308ac21660ada6109b5712640aeba2fe48ad
mkx173 <zhw173@g.ucla.edu>
2024-04-22 23:17:06 -0700

    Migrate FragmentTab to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/FragmentTab.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/FragmentTab.kt
@@ -1,81 +1,71 @@
-package gm.tieba.tabswitch.hooker.eliminate;
+package gm.tieba.tabswitch.hooker.eliminate
 
-import androidx.annotation.NonNull;
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.dao.AcRules.findRule
+import gm.tieba.tabswitch.dao.Preferences.getBoolean
+import gm.tieba.tabswitch.hooker.IHooker
+import gm.tieba.tabswitch.hooker.Obfuscated
+import gm.tieba.tabswitch.hooker.deobfuscation.Matcher
+import gm.tieba.tabswitch.hooker.deobfuscation.SmaliMatcher
+import gm.tieba.tabswitch.hooker.deobfuscation.StringMatcher
+import gm.tieba.tabswitch.util.findFirstMethodByExactType
+import gm.tieba.tabswitch.util.setObjectField
+import org.luckypray.dexkit.query.matchers.ClassMatcher
 
-import org.luckypray.dexkit.query.matchers.ClassMatcher;
-
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.dao.AcRules;
-import gm.tieba.tabswitch.dao.Preferences;
-import gm.tieba.tabswitch.hooker.IHooker;
-import gm.tieba.tabswitch.hooker.Obfuscated;
-import gm.tieba.tabswitch.hooker.deobfuscation.Matcher;
-import gm.tieba.tabswitch.hooker.deobfuscation.SmaliMatcher;
-import gm.tieba.tabswitch.hooker.deobfuscation.StringMatcher;
-import gm.tieba.tabswitch.util.ReflectUtils;
-
-public class FragmentTab extends XposedContext implements IHooker, Obfuscated {
-
-    @NonNull
-    @Override
-    public String key() {
-        return "fragment_tab";
+class FragmentTab : XposedContext(), IHooker, Obfuscated {
+    override fun key(): String {
+        return "fragment_tab"
     }
 
-    @Override
-    public List<? extends Matcher> matchers() {
-        return List.of(
-                new StringMatcher("has_show_message_tab_tips"),
-                new SmaliMatcher("Lcom/airbnb/lottie/LottieAnimationView;->setImageResource(I)V")
-                        .setBaseClassMatcher(ClassMatcher.create().usingStrings("has_show_message_tab_tips"))
-        );
+    override fun matchers(): List<Matcher> {
+        return listOf(
+            StringMatcher("has_show_message_tab_tips"),
+            SmaliMatcher("Lcom/airbnb/lottie/LottieAnimationView;->setImageResource(I)V").apply {
+                classMatcher = ClassMatcher.create().usingStrings("has_show_message_tab_tips")
+            }
+        )
     }
 
-    @Override
-    public void hook() throws Throwable {
-        AcRules.findRule("has_show_message_tab_tips", (matcher, clazz, method) -> {
-            final var md = ReflectUtils.findFirstMethodByExactType(clazz, ArrayList.class);
-            XposedBridge.hookMethod(md, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                    final var tabsToRemove = new HashSet<String>();
-                    if (Preferences.getBoolean("home_recommend")) {
-                        tabsToRemove.add("com.baidu.tieba.homepage.framework.RecommendFrsDelegateStatic");
+    @Throws(Throwable::class)
+    override fun hook() {
+        findRule("has_show_message_tab_tips") { _, clazz, _ ->
+            val method = findFirstMethodByExactType(clazz, ArrayList::class.java)
+            hookBeforeMethod(method) { param ->
+                val tabsToRemove = HashSet<String>().apply {
+                    if (getBoolean("home_recommend")) {
+                        add("com.baidu.tieba.homepage.framework.RecommendFrsDelegateStatic")
                     }
-                    if (Preferences.getBoolean("enter_forum")) {
-                        tabsToRemove.add("com.baidu.tieba.enterForum.home.EnterForumDelegateStatic");
+                    if (getBoolean("enter_forum")) {
+                        add("com.baidu.tieba.enterForum.home.EnterForumDelegateStatic")
                     }
-                    if (Preferences.getBoolean("write_thread")) {
-                        tabsToRemove.add("com.baidu.tieba.write.bottomButton.WriteThreadDelegateStatic");
-                        AcRules.findRule("Lcom/airbnb/lottie/LottieAnimationView;->setImageResource(I)V", (matcher, clazz, method) -> {
-                            Method md = XposedHelpers.findMethodExactIfExists(clazz, sClassLoader, method);
-                            if (md != null) {
-                                XposedBridge.hookMethod(md, new XC_MethodHook() {
-                                    @Override
-                                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                        ReflectUtils.setObjectField(param.thisObject, "com.baidu.tbadk.widget.lottie.TBLottieAnimationView", null);
-                                        param.setResult(null);
-                                    }
-                                });
+                    if (getBoolean("write_thread")) {
+                        add("com.baidu.tieba.write.bottomButton.WriteThreadDelegateStatic")
+                        findRule("Lcom/airbnb/lottie/LottieAnimationView;->setImageResource(I)V") { _, clazz, method ->
+                            val md = XposedHelpers.findMethodExactIfExists(clazz, sClassLoader, method)
+                            md?.let {
+                                hookBeforeMethod(md) {param ->
+                                    setObjectField(
+                                        param.thisObject,
+                                        "com.baidu.tbadk.widget.lottie.TBLottieAnimationView",
+                                        null
+                                    )
+                                    param.setResult(null)
+                                }
                             }
-                        });
+                        }
                     }
-                    if (Preferences.getBoolean("im_message")) {
-                        tabsToRemove.add("com.baidu.tieba.imMessageCenter.im.chat.notify.ImMessageCenterDelegateStatic");
-                        tabsToRemove.add("com.baidu.tieba.immessagecenter.im.chat.notify.ImMessageCenterDelegateStatic");
+                    if (getBoolean("im_message")) {
+                        add("com.baidu.tieba.imMessageCenter.im.chat.notify.ImMessageCenterDelegateStatic")
+                        add("com.baidu.tieba.immessagecenter.im.chat.notify.ImMessageCenterDelegateStatic")
                     }
-                    final var list = (ArrayList<?>) param.args[0];
-                    list.removeIf(tab -> tabsToRemove.contains(tab.getClass().getName()));
                 }
-            });
-        });
+
+                val list = param.args[0] as? ArrayList<*>
+                list?.removeIf { tab: Any -> tabsToRemove.contains(tab.javaClass.getName()) }
+            }
+        }
     }
 }
