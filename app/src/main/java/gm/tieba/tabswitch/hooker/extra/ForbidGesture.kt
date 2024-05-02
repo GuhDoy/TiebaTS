28770685f6ecc271c9c85e18687d39e3443c2e5c
mkx173 <zhw173@g.ucla.edu>
2024-05-02 03:32:10 -0700

    Migrate ForbidGesture to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/extra/ForbidGesture.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/extra/ForbidGesture.kt
@@ -1,83 +1,71 @@
-package gm.tieba.tabswitch.hooker.extra;
+package gm.tieba.tabswitch.hooker.extra
 
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.ViewGroup;
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.dao.AcRules
+import gm.tieba.tabswitch.dao.AcRules.findRule
+import gm.tieba.tabswitch.hooker.IHooker
+import gm.tieba.tabswitch.hooker.Obfuscated
+import gm.tieba.tabswitch.hooker.deobfuscation.Matcher
+import gm.tieba.tabswitch.hooker.deobfuscation.ResMatcher
+import gm.tieba.tabswitch.hooker.deobfuscation.SmaliMatcher
+import gm.tieba.tabswitch.util.getObjectField
+import gm.tieba.tabswitch.util.getR
+import org.luckypray.dexkit.query.matchers.ClassMatcher
 
-import androidx.annotation.NonNull;
-
-import java.lang.reflect.Method;
-import java.util.List;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.dao.AcRules;
-import gm.tieba.tabswitch.hooker.IHooker;
-import gm.tieba.tabswitch.hooker.Obfuscated;
-import gm.tieba.tabswitch.hooker.deobfuscation.Matcher;
-import gm.tieba.tabswitch.hooker.deobfuscation.ResMatcher;
-import gm.tieba.tabswitch.util.ReflectUtils;
-
-public class ForbidGesture extends XposedContext implements IHooker, Obfuscated {
-
-    @NonNull
-    @Override
-    public String key() {
-        return "forbid_gesture";
+class ForbidGesture : XposedContext(), IHooker, Obfuscated {
+    override fun key(): String {
+        return "forbid_gesture"
     }
 
-    @Override
-    public List<? extends Matcher> matchers() {
-        return List.of(new ResMatcher(ReflectUtils.getR("drawable", "icon_word_t_size"), "forbid_gesture"));
+    override fun matchers(): List<Matcher> {
+        return listOf(
+            ResMatcher(getR("drawable", "icon_word_t_size").toLong(), "forbid_gesture"),
+            SmaliMatcher("Ljava/lang/Math;->sqrt(D)D").apply {
+                classMatcher = ClassMatcher.create().className("com.baidu.tbadk.widget.DragImageView")
+            }
+        )
     }
 
-    @Override
-    public void hook() throws Throwable {
+    @SuppressLint("ClickableViewAccessibility")
+    @Throws(Throwable::class)
+    override fun hook() {
         // 帖子字号
-        AcRules.findRule("forbid_gesture", (AcRules.Callback) (matcher, clazz, method) ->
-                XposedHelpers.findAndHookMethod(clazz, sClassLoader, method, XC_MethodReplacement.returnConstant(null))
-        );
-        // 视频帖字号
-        XposedHelpers.findAndHookMethod("com.baidu.tieba.pb.videopb.fragment.DetailInfoAndReplyFragment", sClassLoader,
-                "onCreateView", LayoutInflater.class, ViewGroup.class, Bundle.class, new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
-                        final ViewGroup recyclerView = (ViewGroup) ReflectUtils.getObjectField(param.thisObject,
-                                "com.baidu.adp.widget.ListView.BdTypeRecyclerView");
-                        recyclerView.setOnTouchListener((v, event) -> false);
-                    }
-                });
-        // 帖子进吧
-        XposedHelpers.findAndHookMethod("com.baidu.tieba.pb.pb.main.PbLandscapeListView", sClassLoader,
-                "dispatchTouchEvent", MotionEvent.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(final XC_MethodHook.MethodHookParam param) throws Throwable {
-                        XposedHelpers.callMethod(param.thisObject, "setForbidDragListener", true);
-                    }
-                });
-        // 图片缩放倍数
-        final Class<?> clazz = XposedHelpers.findClass("com.baidu.tbadk.widget.DragImageView", sClassLoader);
-        Method method;
-        try {
-            method = clazz.getDeclaredMethod("getMaxScaleValue", Bitmap.class);
-        } catch (final NoSuchMethodException e) {
-            try {
-                method = clazz.getDeclaredMethod("U", Bitmap.class);
-            } catch (final NoSuchMethodException e2) {
-                // 12.47.0.2+
-                method = clazz.getDeclaredMethod("V", Bitmap.class);
+        findRule(matchers()) { matcher, clazz, method ->
+            when (matcher) {
+                "forbid_gesture" -> hookReplaceMethod(clazz, method) { null }
+                "Ljava/lang/Math;->sqrt(D)D" -> hookAfterMethod(clazz, method, Bitmap::class.java) { param ->
+                    param.result = 3 * param.result as Float
+                }
             }
         }
-        XposedBridge.hookMethod(method, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(final XC_MethodHook.MethodHookParam param) throws Throwable {
-                param.setResult(3 * (float) param.getResult());
-            }
-        });
+
+        // 视频帖字号
+        hookAfterMethod("com.baidu.tieba.pb.videopb.fragment.DetailInfoAndReplyFragment",
+            "onCreateView", LayoutInflater::class.java, ViewGroup::class.java, Bundle::class.java) { param ->
+            val recyclerView = getObjectField(
+                param.thisObject,
+                "com.baidu.adp.widget.ListView.BdTypeRecyclerView"
+            ) as? ViewGroup
+            recyclerView?.setOnTouchListener { _, _ -> false }
+        }
+
+        // 帖子进吧
+        hookBeforeMethod(
+            "com.baidu.tieba.pb.pb.main.PbLandscapeListView",
+            "dispatchTouchEvent", MotionEvent::class.java
+        ) { param ->
+            XposedHelpers.callMethod(param.thisObject, "setForbidDragListener", true)
+        }
     }
 }
