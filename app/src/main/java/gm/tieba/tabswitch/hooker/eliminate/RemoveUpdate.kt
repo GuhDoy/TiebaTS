1759e88263dbd5bb94991a4d3319c79d7422cc7e
mkx173 <zhw173@g.ucla.edu>
2024-05-01 19:26:08 -0700

    Migrate RemoveUpdate to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/RemoveUpdate.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/RemoveUpdate.kt
@@ -1,26 +1,22 @@
-package gm.tieba.tabswitch.hooker.eliminate;
+package gm.tieba.tabswitch.hooker.eliminate
 
-import androidx.annotation.NonNull;
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.hooker.IHooker
+import org.json.JSONObject
 
-import org.json.JSONObject;
-
-import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.hooker.IHooker;
-
-public class RemoveUpdate extends XposedContext implements IHooker {
-
-    @NonNull
-    @Override
-    public String key() {
-        return "remove_update";
+class RemoveUpdate : XposedContext(), IHooker {
+    override fun key(): String {
+        return "remove_update"
     }
 
-    @Override
-    public void hook() throws Throwable {
+    @Throws(Throwable::class)
+    override fun hook() {
         //Lcom/baidu/tbadk/coreExtra/data/VersionData;->parserJson(Lorg/json/JSONObject;)V
-        XposedHelpers.findAndHookMethod("com.baidu.tbadk.coreExtra.data.VersionData", sClassLoader,
-                "parserJson", JSONObject.class, XC_MethodReplacement.returnConstant(null));
+        hookReplaceMethod(
+            "com.baidu.tbadk.coreExtra.data.VersionData",
+            "parserJson", JSONObject::class.java
+        ) { null }
     }
 }
