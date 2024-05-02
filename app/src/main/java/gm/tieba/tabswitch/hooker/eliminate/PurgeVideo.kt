c0a1b389a8cdaf41c86e3d9948e1e1e6f7ff7460
mkx173 <zhw173@g.ucla.edu>
2024-05-01 19:07:17 -0700

    Migrate PurgeVideo to Kotlin

--- app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PurgeVideo.kt
+++ app/src/main/java/gm/tieba/tabswitch/hooker/eliminate/PurgeVideo.kt
@@ -1,30 +1,23 @@
-package gm.tieba.tabswitch.hooker.eliminate;
+package gm.tieba.tabswitch.hooker.eliminate
 
-import androidx.annotation.NonNull;
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import gm.tieba.tabswitch.XposedContext
+import gm.tieba.tabswitch.hooker.IHooker
 
-import java.util.List;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
-import gm.tieba.tabswitch.XposedContext;
-import gm.tieba.tabswitch.hooker.IHooker;
-
-public class PurgeVideo extends XposedContext implements IHooker {
-    @NonNull
-    @Override
-    public String key() {
-        return "purge_video";
+class PurgeVideo : XposedContext(), IHooker {
+    override fun key(): String {
+        return "purge_video"
     }
 
-    @Override
-    public void hook() throws Throwable {
-        XposedHelpers.findAndHookMethod("tbclient.Personalized.DataRes$Builder", sClassLoader, "build", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                final List<?> threadList = (List<?>) XposedHelpers.getObjectField(param.thisObject, "thread_list");
-                if (threadList == null) return;
-                threadList.removeIf(o -> XposedHelpers.getObjectField(o, "video_info") != null);
-            }
-        });
+    @Throws(Throwable::class)
+    override fun hook() {
+        hookBeforeMethod(
+            "tbclient.Personalized.DataRes\$Builder",
+            "build", Boolean::class.javaPrimitiveType
+        ) { param ->
+            val threadList = XposedHelpers.getObjectField(param.thisObject, "thread_list") as? MutableList<*>
+            threadList?.removeIf { o: Any? -> XposedHelpers.getObjectField(o, "video_info") != null }
+        }
     }
 }
