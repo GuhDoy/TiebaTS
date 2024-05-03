dd4bb6a7edd596f5af50cc76ee51b6482a4674f8
mkx173 <zhw173@g.ucla.edu>
2024-05-03 04:14:52 -0700

    Remove project wide build.gradle.kts

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
-}
