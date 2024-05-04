a63471ee6735ff6072d95a3e8d3a35b2c6443f6d
mkx173 <zhw173@g.ucla.edu>
2024-05-03 17:51:31 -0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,11 @@ android {
         minSdk = 28
         targetSdk = sdk
         versionCode = gitCommitCount
-        versionName = "3.0.0-alpha"
+        versionName = "3.0.0-beta"
         if (versionName!!.contains("alpha") || versionName!!.contains("beta")) {
             versionNameSuffix = ".$commitCountSinceLatestTag"
         }
-        buildConfigField("String", "TARGET_VERSION", "\"12.59.1.0\"")
+        buildConfigField("String", "TARGET_VERSION", "\"12.60.1.1\"")
         buildConfigField("String", "MIN_VERSION", "\"12.53.1.0\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
