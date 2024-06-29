31945a5f2e41865ad6573f2942d9c18c64ed7dbc
mkx173 <zhw173@g.ucla.edu>
2024-06-28 21:07:14 -0700

    chore: update AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,11 @@ android {
         minSdk = 28
         targetSdk = sdk
         versionCode = gitCommitCount
-        versionName = "3.0.1"
+        versionName = "3.0.2"
         if (versionName!!.contains("alpha") || versionName!!.contains("beta")) {
             versionNameSuffix = ".$commitCountSinceLatestTag"
         }
-        buildConfigField("String", "TARGET_VERSION", "\"12.60.4.1\"")
+        buildConfigField("String", "TARGET_VERSION", "\"12.64.0.0\"")
         buildConfigField("String", "MIN_VERSION", "\"12.53.1.0\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
